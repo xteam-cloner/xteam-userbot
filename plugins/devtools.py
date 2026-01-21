31295ea9a348b258b85f227922b63603a0fc8cb6
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2026-01-21 12:58:59 +0700

    Remove input from final output formatting
    Signed-off-by: teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>

--- plugins/devtools.py
+++ plugins/devtools.py
@@ -190,7 +190,7 @@ async def _(event):
     tmt = tima * 1000
     timef = time_formatter(tmt)
     timeform = timef if not timef == "0s" else f"{tmt:.3f}µs"
-    final_output = "<blockquote>Input:\n{}</blockquote>\n<blockquote>Output:\n{}</blockquote>\n<blockquote>Timsec:{}\n</blockquote>".format(
+    final_output = "<blockquote>Output:\n{}</blockquote>\n<blockquote>Timsec:{}\n</blockquote>".format(
         cmd,
         evaluation,
         timeform,
